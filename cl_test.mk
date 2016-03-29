1533f80867ded84434e524923766d2f854b0ce1d
Sam Anderson <sam@lime-lab.com>
2016-03-29 15:50:47 -0700

    wip

--- cl_test.mk
+++ cl_test.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-CL_TEST_VERSION = 481
-CL_TEST_SITE = $(BR2_GNU_MIRROR)/less
-CL_TEST_LICENSE = GPLv3+
+CL_TEST_VERSION = master
+CL_TEST_SITE = $(call github,limelabsam,cl_test,$(CL_TEST_VERSION))
+#CL_TEST_LICENSE = GPLv3+
 CL_TEST_LICENSE_FILES = COPYING
 # Build after busybox, full-blown is better
-CL_TEST_DEPENDENCIES = ncurses $(if $(BR2_PACKAGE_BUSYBOX),busybox)
+#CL_TEST_DEPENDENCIES = ncurses $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 
 define CL_TEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/usr/bin/helloworld
+	echo $(INSTALL) -m 0755 $(@D)/helloworld $(TARGET_DIR)/usr/bin/helloworld
 endef
 
-$(eval $(autotools-package))
+#$(eval $(autotools-package))
